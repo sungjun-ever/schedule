f60c69532ab0c8ec37b704a1fb9cd273e34fa315
임성준 <zkfmak10@gmail.com>
2025-04-13 13:26:17 +0900

    UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Enum\User\UserLevel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 /**
@@ -26,9 +28,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => static::$password ??= Hash::make('123456'),
+            'level' => 'user',
         ];
     }
 
